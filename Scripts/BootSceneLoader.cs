a418fcdfe4992320e561189c5d6ae85b70e5b223
Parris Khachi <parris@storyforj.com>
2020-03-07 08:19:53 -0800

    Temporarily remove support for saving/retrieving of data

--- Scripts/BootSceneLoader.cs
+++ Scripts/BootSceneLoader.cs
@@ -8,7 +8,6 @@ using UnityEngine.SceneManagement;
 
 public class BootSceneLoader : MonoBehaviour
 {
-    public delegate void Callback(System.IntPtr ptr);
 
     [DllImport("__Internal")]
     private static extern void ODOTrigger(string triggerName);
@@ -16,38 +15,10 @@ public class BootSceneLoader : MonoBehaviour
     [DllImport("__Internal")]
     private static extern void ODOTrack(string eventName, string jsonData);
 
-    [DllImport("__Internal")]
-    private static extern void ODOSave(string key, string jsonData);
-
-    [DllImport("__Internal")]
-    private static extern void ODOSave(string key, string jsonData, Callback callback);
-
-    [DllImport("__Internal")]
-    private static extern void ODOSaveForPlayer(string key, string jsonData);
-
-    [DllImport("__Internal")]
-    private static extern void ODOSaveForPlayer(string key, string jsonData, Callback callback = null);
-
-    [DllImport("__Internal")]
-    private static extern void ODOGet(string key, Callback callback);
-
-    [DllImport("__Internal")]
-    private static extern void ODOGetForPlayer(string key, Callback callback);
-
     // Start is called before the first frame update
     void Start()
     {
         ODOTrigger("ready");
-        ODOSave("test", "{ hi: 2 }");
-        ODOGet("test", GetCallback);
-    }
-
-    [MonoPInvokeCallback(typeof(Callback))]
-    public static void GetCallback(System.IntPtr ptr)
-    {
-        string data = Marshal.PtrToStringAuto(ptr);
-        // I would take a look at JsonUtility.FromJson to deserialize
-        Debug.Log(data);
     }
 
     void ODOStart() {
